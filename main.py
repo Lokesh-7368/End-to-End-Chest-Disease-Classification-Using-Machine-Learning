22dd02bf9d70e83ee4aaff39f8ad59fc014fcfd9
Lokesh-7368 <lokeshbhambhu620@gmail.com>
2026-02-24 01:44:32 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 ## from src.ChestDiseaseClassification import logger
-from ChestDiseaseClassification import logger
+## from ChestDiseaseClassification import logger
 
-logger.info("Welcome to Chest Disease Classification Project")
+## logger.info("Welcome to Chest Disease Classification Project")
+
+from ChestDiseaseClassification import logger
+from ChestDiseaseClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
